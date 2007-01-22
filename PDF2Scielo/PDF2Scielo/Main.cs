b4b62c02d68d6e1577ea82e7d32b268f5573aac1
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-01-22 22:53:20 +0000

    Commit inicial de la aplicacion pdf2scielo y la libreria PDF2Text.
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@5 85877316-7822-0410-bcce-81226501b1f5

---
+++ PDF2Scielo/PDF2Scielo/Main.cs
@@ -0,0 +1,13 @@
+// project created on 1/22/2007 at 4:46 PM
+using System;
+
+namespace PDF2Scielo
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+		}
+	}
+}
---
+++ PDF2Scielo/PDF2Text/MyClass.cs
@@ -0,0 +1,7 @@
+// project created on 1/22/2007 at 4:46 PM
+using System;
+
+public class MyClass
+{
+
+}
