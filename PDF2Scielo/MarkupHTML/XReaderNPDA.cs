33bed7496995e40ad3ed4ccafc352967d466804c
Virginia Teodosio Procopio <lunysska@gmail.com>
2007-01-31 01:04:34 +0000

    this class reads the full definition of automaton contains in a xml file
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@21 85877316-7822-0410-bcce-81226501b1f5

---
+++ PDF2Scielo/MarkupHTML/XReaderNPDA.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Xml;
+using System.Collections;
+
+using System.IO;
+
+namespace Scielo{
+namespace MarkupHTML{
+
+/* this class implements the read from an XML file which one contents the formal definition of an specific NPDA 
+ and divide and organized in classes all the formal elements of NPDA */ 
+
+public class XReaderNPDA{
+
+
+    public XmlNodeList deltaElements;
+    public XmlNode deltaTransitions [];
+
+   /**
+    * This constructor reads the name file that contains the definition of the NPDA. 
+    */
+   public XReaderNPDA( string namefile ){
+
+      XmlDocument document = new XmlDocument();
+      document.Load( namefile );
+      
+      XmlNode root = document.DocumentElement;
+      deltaElements = root.SelectNodes("/pda/delta/trans");
+      Console.WriteLine("en la lista=="+deltaElements.Count);
+ 
+   }
+
+   /* This method gets the full Delta Relation defined in the XML document in the form of list of nodes
+   */
+   public void getDeltaRelation(){
+      	
+      int count = 1;  	
+      foreach (XmlNode alph2 in deltaElements){
+
+        XmlNodeList listTmp = alph2.ChildNodes;
+        Console.WriteLine("elemento=="+count);
+         
+        foreach (XmlNode alph3 in listTmp){
+          Console.WriteLine("::"+alph3.FirstChild.Value);
+        }      
+        count ++;
+      }
+   }
+
+   public static void Main(){
+
+      ReaderXMLNPDA rXN = new ReaderXMLNPDA("npdaAtm.xml");
+      rXN.getDeltaRelation();
+   }
+  
+
+}
+}
+}
