3727349f87fe87c89fd0e9264549fd43f48b6952
Virginia Teodosio Procopio <lunysska@gmail.com>
2007-01-31 01:06:51 +0000

    el token para la cadena de entrada
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@22 85877316-7822-0410-bcce-81226501b1f5

---
+++ PDF2Scielo/MarkupHTML/ParserHTML.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+
+namespace Scielo{
+namespace MarkupHTML{
+
+public class ParserHTML : IEnumerable
+{
+   private string[] elements;
+
+   public ParserHTML(string source, char[] delimiters)
+   {
+      // Parse the string into tokens:
+      elements = source.Split(delimiters);
+   }
+
+   // IEnumerable Interface Implementation:
+   //   Declaration of the GetEnumerator() method 
+   //   required by IEnumerable
+   public IEnumerator GetEnumerator()
+   {
+      return new TokenEnumerator(this);
+   }
+
+   // Inner class implements IEnumerator interface:
+   private class TokenEnumerator : IEnumerator
+   {
+      private int position = -1;
+      private ParserHTML t;
+
+      public TokenEnumerator(ParserHTML t)
+      {
+         this.t = t;
+      }
+
+      // Declare the MoveNext method required by IEnumerator:
+      public bool MoveNext()
+      {
+         if (position < t.elements.Length - 1)
+         {
+            position++;
+            return true;
+         }
+         else
+         {
+            return false;
+         }
+      }
+
+      // Declare the Reset method required by IEnumerator:
+      public void Reset()
+      {
+         position = -1;
+      }
+
+      // Declare the Current property required by IEnumerator:
+      public object Current
+      {
+         get
+         {
+            return t.elements[position];
+         }
+      }
+   }
+   
+}
+}
+}
