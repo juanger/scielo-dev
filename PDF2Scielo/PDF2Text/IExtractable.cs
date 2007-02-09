147cf5d58781dd20487f2d166faee7fd3a7ee12b
Virginia Teodosio Procopio <lunysska@gmail.com>
2007-02-09 01:06:56 +0000

    * PDF2Text/PDF2Text.mdp, PDF2Text/IExtractable.cs,   PDF2Scielo/PDF2Scielo.mdp: Commit inicial de la Interfaz   IExtractable
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@34 85877316-7822-0410-bcce-81226501b1f5

---
+++ PDF2Scielo/PDF2Text/IExtractable.cs
@@ -0,0 +1,26 @@
+//
+// AssemblyInfo.cs: Assembly Information.
+//
+// Author:
+//   Hector E. Gomez Morales (hectoregm@gmail.com)
+//   Anaid V. Velazquez Rivera (anaidv@gmail.com)
+//   Alejandro Rosendo Robles. (rosendo69@hotmail.com)
+//   Virginia Teodosio Procopio. (ainigriv_t@hotmail.com)
+//
+// Copyright (C) 2007 UNAM DGB
+//
+
+using System;
+using System.Collections;
+
+namespace Scielo {
+namespace PDF2Text {
+	
+public interface IExtractable {
+
+	String GetText (string encoding);
+	
+	Queue GetNonText ();
+}
+}
+}
