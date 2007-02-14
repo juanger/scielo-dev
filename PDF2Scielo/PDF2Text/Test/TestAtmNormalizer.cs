e1b590986eac07a9befe32dfea32c528e4af578e
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-02-14 21:33:51 +0000

    * PDF2Text/Test/TestAtmNormalizer.cs, PDF2Text/PDF2Text.mdp: Commit   inicial de TestAtmNormalizer.
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@38 85877316-7822-0410-bcce-81226501b1f5

---
+++ PDF2Scielo/PDF2Text/Test/TestAtmNormalizer.cs
@@ -0,0 +1,28 @@
+//
+// AssemblyInfo.cs: Assembly Information.
+//
+// Author:
+//   Hector E. Gomez Morales (hectoregm@gmail.com)
+//   Anaid V. Velazquez Rivera (anaidv@gmail.com)
+//   Virginia Teodosio Procopio (ainigriv_t@hotmail.com)
+//   Alejandro Rosendo Robles (rosendo69@hotmail.com)
+//
+//
+// Copyright (C) 2007 UNAM DGB
+//
+
+using System;
+using NUnit.Framework;
+
+namespace Scielo {
+namespace PDF2Text {
+
+[TestFixture()]
+public class TestAtmNormalizer {
+	
+	public void TestCase ()
+	{
+	}
+}
+}
+}
