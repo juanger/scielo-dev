0739e4e52790719152079545fc59450940b734d4
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-02-21 23:30:20 +0000

    * PDF2Text/Test/TestPDFPoppler.cs, PDF2Scielo.mds: Agregando PDFImage a   la solucion PDF2Scielo
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@55 85877316-7822-0410-bcce-81226501b1f5

--- PDF2Scielo/PDF2Text/Test/TestPDFPoppler.cs
+++ PDF2Scielo/PDF2Text/Test/TestPDFPoppler.cs
@@ -51,6 +51,25 @@ public class TestPDFPoppler {
 		Assert.IsNotNull (doc1, "CI02:2");
 		Assert.IsNotNull (doc2, "CI02:3");
 	}
+	
+	[Test]
+	public void GetText ()
+	{
+		string path, path0, path1, path2; 
+		path = Environment.CurrentDirectory;
+		path = path.Replace ("bin/Debug", String.Empty);
+		path0 = Path.Combine (path, "Test/v17n01a02.pdf");
+		path1 = Path.Combine (path, "Test/v17n4a03.pdf");
+		path2 = Path.Combine (path, "Test/v18n4a02.pdf");
+		
+		Uri uri0 = new Uri (path0);
+		Uri uri1 = new Uri (path1);
+		Uri uri2 = new Uri (path2);
+		
+		PDFPoppler doc0 = PDFPoppler.CreateInstance (uri0);
+		PDFPoppler doc1 = PDFPoppler.CreateInstance (uri1);
+		PDFPoppler doc2 = PDFPoppler.CreateInstance (uri2);
+	}
 }
 }
 }
