02f4645d30fdbb76b83335b17aa22cd9c83c5b12
Virginia Teodosio Procopio <lunysska@gmail.com>
2007-02-23 01:59:07 +0000

    agregamos el TestStringEncoding
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@68 85877316-7822-0410-bcce-81226501b1f5

--- PDF2Scielo/Utils/Test/TestStringEncoding.cs
+++ PDF2Scielo/Utils/Test/TestStringEncoding.cs
@@ -1,3 +1,15 @@
+//
+// TestStringEncoding.cs: Test for StringEncoding class
+//
+// Author:
+//   Hector E. Gomez Morales (hectoregm@gmail.com)
+//   Anaid V. Velazquez Rivera (anaidv@gmail.com)
+//   Virginia Teodosio Procopio (ainigriv_t@hotmail.com)
+//   Alejandro Rosendo Robles (rosendo69@hotmail.com)
+//
+//
+// Copyright (C) 2007 UNAM DGB
+//
 
 using System;
 using NUnit.Framework;
