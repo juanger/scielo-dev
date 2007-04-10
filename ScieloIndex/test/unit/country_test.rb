5ccd4be30821d075603105e5c6c14fde693af765
Virginia Teodosio Procopio <lunysska@gmail.com>
2007-04-10 01:21:46 +0000

    Pruebas unitarias (Rigth) para el modelo de countries
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@156 85877316-7822-0410-bcce-81226501b1f5

--- ScieloIndex/test/unit/country_test.rb
+++ ScieloIndex/test/unit/country_test.rb
@@ -2,10 +2,45 @@ require File.dirname(__FILE__) + '/../test_helper'
 
 class CountryTest < Test::Unit::TestCase
   fixtures :countries
+  
+  def setup
+    @countries = [:mexico, :brasil]
+  end
 
   # Replace this with your real tests.
-  def test_mx
-    @country = countries(:mexico)
-    assert_equal @country.name, 'México'
+  def test_created_countries_from_fixtures
+    @countries.each { |country|
+      @country = countries(country)
+      @country_db = Country.find_by_name(@country.name)
+      assert_equal @country.name, @country_db.name
+      assert_equal @country.id, @country_db.id
+      assert_equal @country.code, @country_db.code
+    }
   end
+
+  def test_update_countries
+    @countries.each { |country|
+      @country = countries(country)
+      @country_db = Country.find_by_name(@country.name)
+      @country_db.name.reverse
+      assert @country_db.update
+      @country_db.id = @country_db.id + 1
+      assert @country_db.update
+      @country_db.code.reverse
+      assert @country_db.update
+      #puts @country_db.code.reverse
+    }
+   end
+
+  def test_delete_countries
+    @countries.each { |country|
+      @country = countries(country)
+      @country_db = Country.find_by_name(@country.name)
+      assert @country_db.destroy
+      @country_db = Country.find_by_name(@country.name)
+      assert @country_db.nil?
+    }
+  end
+  #def teardown
+  #end
 end
