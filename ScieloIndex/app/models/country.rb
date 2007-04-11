faf5f02352480833ba558ddc69e77954ac777f0a
Virginia Teodosio Procopio <lunysska@gmail.com>
2007-04-11 01:13:35 +0000

    Se validaron los campos name y code con el fin de almacenar la informacion en la base de datos con un formato especifico.
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@157 85877316-7822-0410-bcce-81226501b1f5

--- ScieloIndex/app/models/country.rb
+++ ScieloIndex/app/models/country.rb
@@ -1,4 +1,9 @@
 class Country < ActiveRecord::Base
-  validates_numericality_of :id
-  validates_presence_of :id,:name,:code
+  #validates_numericality_of :id
+  validates_format_of :id, :with => /^(\d){3}$/
+  validates_presence_of :id,:name,:code 
+  validates_length_of :name, :within => 3..80, :allow_nil => false
+  validates_length_of :code, :within => 2..3, :allow_nil => false
+  #validates_format_of :name, :code :with => /^[a-z]$/ 
+  validates_uniqueness_of :name,:code, :scope => :id
 end
