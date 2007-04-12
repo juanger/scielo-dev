478ab5c7182012f0c96f656361b95f78ec58d96b
Virginia Teodosio Procopio <lunysska@gmail.com>
2007-04-12 00:46:08 +0000

    agregamos mas validaciones para el campo de id y de nombre
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@159 85877316-7822-0410-bcce-81226501b1f5

--- ScieloIndex/app/models/country.rb
+++ ScieloIndex/app/models/country.rb
@@ -1,9 +1,10 @@
 class Country < ActiveRecord::Base
   #validates_numericality_of :id
-  validates_format_of :id, :with => /^(\d){3}$/
   validates_presence_of :id,:name,:code 
   validates_length_of :name, :within => 3..80, :allow_nil => false
   validates_length_of :code, :within => 2..3, :allow_nil => false
-  #validates_format_of :name, :code :with => /^[a-z]$/ 
-  validates_uniqueness_of :name,:code, :scope => :id
+  validates_format_of :id, :with => /^(\d){1,3}$/, :allow_nil => false
+  validates_format_of :name, :with => /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]{3,80}$/
+  validates_format_of :code, :with => /^[a-zA-Z]{2,3}$/
+  validates_uniqueness_of :name,:code,:id
 end
