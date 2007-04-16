8e61cbd1295730624345adee034b784e85e6c549
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-04-16 21:58:06 +0000

    Scalffoldeo del controlador authors
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@168 85877316-7822-0410-bcce-81226501b1f5

--- ScieloIndex/test/functional/authors_controller_test.rb
+++ ScieloIndex/test/functional/authors_controller_test.rb
@@ -5,14 +5,88 @@ require 'authors_controller'
 class AuthorsController; def rescue_action(e) raise e end; end
 
 class AuthorsControllerTest < Test::Unit::TestCase
+  fixtures :authors
+
   def setup
     @controller = AuthorsController.new
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
+
+    @first_id = authors(:first).id
   end
 
-  # Replace this with your real tests.
-  def test_truth
-    assert true
+  def test_index
+    get :index
+    assert_response :success
+    assert_template 'list'
+  end
+
+  def test_list
+    get :list
+
+    assert_response :success
+    assert_template 'list'
+
+    assert_not_nil assigns(:authors)
+  end
+
+  def test_show
+    get :show, :id => @first_id
+
+    assert_response :success
+    assert_template 'show'
+
+    assert_not_nil assigns(:author)
+    assert assigns(:author).valid?
+  end
+
+  def test_new
+    get :new
+
+    assert_response :success
+    assert_template 'new'
+
+    assert_not_nil assigns(:author)
+  end
+
+  def test_create
+    num_authors = Author.count
+
+    post :create, :author => {}
+
+    assert_response :redirect
+    assert_redirected_to :action => 'list'
+
+    assert_equal num_authors + 1, Author.count
+  end
+
+  def test_edit
+    get :edit, :id => @first_id
+
+    assert_response :success
+    assert_template 'edit'
+
+    assert_not_nil assigns(:author)
+    assert assigns(:author).valid?
+  end
+
+  def test_update
+    post :update, :id => @first_id
+    assert_response :redirect
+    assert_redirected_to :action => 'show', :id => @first_id
+  end
+
+  def test_destroy
+    assert_nothing_raised {
+      Author.find(@first_id)
+    }
+
+    post :destroy, :id => @first_id
+    assert_response :redirect
+    assert_redirected_to :action => 'list'
+
+    assert_raise(ActiveRecord::RecordNotFound) {
+      Author.find(@first_id)
+    }
   end
 end
