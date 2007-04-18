ab8bbbb66c7209dc2c99678bb5d9df6efbd8dc75
Virginia Teodosio Procopio <lunysska@gmail.com>
2007-04-18 01:02:45 +0000

    Agregamos pruebas unitarias para la tabla institutions
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@174 85877316-7822-0410-bcce-81226501b1f5

--- ScieloIndex/app/models/institution.rb
+++ ScieloIndex/app/models/institution.rb
@@ -1,6 +1,6 @@
 class Institution < ActiveRecord::Base
   validates_presence_of :id, :name, :country_id
-  validates_lenght_of :id, :in => 1..99999
-  validates_lenght_of :name, :within => 3..500
+  validates_length_of :id, :in => 1..99999
+  validates_length_of :name, :within => 3..500
   validates_inclusion_of :country_id, :in => 1..999
 end
