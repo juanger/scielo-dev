225ae0bae2b8c84eec4ce01f956d8a81dfca5292
Virginia Teodosio Procopio <lunysska@gmail.com>
2007-05-09 00:51:06 +0000

    Se completo la extraccion de informacion de la BD Aleph para las tablas correspondientes
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@220 85877316-7822-0410-bcce-81226501b1f5

--- ScieloIndex/tools/DataExtractor.rb
+++ ScieloIndex/tools/DataExtractor.rb
@@ -1,5 +1,5 @@
 require "Country"
-class DataExtractor 
+class DataExtractor_new 
   def initialize( nameOpen, nameExit )
     @name_open = nameOpen
     @name_exit = nameExit
@@ -7,6 +7,9 @@ class DataExtractor
     @recordCollection = Array::new()
     @authorLine = ""
     @institutionLine = ""
+    @articleLine = ""
+    @journalLine = ""
+    @journal_issueLine = ""
   end # of unitialize
   
   def preProcessingFile( )
@@ -63,8 +66,14 @@ class DataExtractor
     @recordCollection.each{ |record|
       getDataAuthor( record )
       getDataInstitution( record )
+      getDataArticle( record )
+      getDataJournal( record )
+      getDataJournalIssue( record )
       puts "#{@authorLine}"
       puts "#{@institutionLine}"
+      puts "#{@articleLine}"
+      puts "#{@journalLine}"
+      puts "#{@journal_issueLine}"
     }
   end
   
@@ -105,13 +114,86 @@ class DataExtractor
             countryID = country.id
           end
         }
+        
         @institutionLine = "INSERT INTO institutions VALUES ('"+name+"', '', '', , '',#{countryID}, '', '', '', '','','');"
       end
     }
   end
+
+  def getDataArticle( record)
+    title = ""
+    page_range = ""
+    record.each { |element|
+      key = element[0]
+      value = element[1]
+      if key == "300" && value =~ /\$\$e/
+        content = value.split("\$\$e")
+        page_range = content[1]
+      end
+      if key == "245" && value =~ /\$\$a/
+        value = value.gsub("\$\$a","")
+        title = value
+      end
+      @articleLine = "INSERT INTO articles VALUES ('"+title+"', '', '', '"+page_range+"', '', '', '');"
+    }
+  end
+  
+  def getDataJournal( record )
+    issn = ""
+    record.each {|element|
+      key = element[0]
+      value = element[1]
+      if key == "022" && value =~ /\$\$a/
+        value = value.gsub("\$\$a","")
+        issn = value
+        @journalLine = "INSERT INTO journals VALUES ('','"+issn+"', '');"
+      end
+    }
+  end
+
+  def getDataJournalIssue( record )
+    number = ""
+    volume = ""
+    content = ""
+    year = -1
+    record.each {|element|
+      key = element[0]
+      value = element[1]
+      if key == "300" 
+        if value =~ /\$\$b/
+          content = value.split("\$\$b")
+          content2 = content[1].split("\$\$")
+          number = content2[0]
+        end
+        if value =~ /\$\$a/
+          volume = content[0].gsub("\$\$a","")
+        end
+      end
+      if key == "260" && value =~ /\$\$b/
+        value = value.gsub("\$\$b","")
+        year = value
+      end
+      @journal_issueLine = "INSERT INTO journal_issues VALUES ('','"+number+"', '"+volume+"', #{year});"
+    }
+  end
+
+  def getDataCollection( record )
+    title = ""
+    record.each {|element|
+      key = element[0]
+      value = element[1]
+      if key == "222" && value =~ /\$\$b/
+        content = value.split("\$\$b")
+        content2 = content[1].split("\$\$")
+        number = content2[0]
+        @journal_issueLine = "INSERT INTO journal_issues VALUES ('','"+number+"', '"+volume+"', #{year});"
+      end
+      }
+  end
+  
 end
 
-data1 = DataExtractor.new("../../../dataScieloIndex/dataDAT/clase30tmp.txt", "clase26abrExtract.txt")
+data1 = DataExtractor_new.new("../../../dataScieloIndex/dataDAT/clase30tmp.txt", "clase26abrExtract.txt")
 data1.preProcessingFile( )
 data1.collectionRecord( )
 data1.preProcessingCollection( )
