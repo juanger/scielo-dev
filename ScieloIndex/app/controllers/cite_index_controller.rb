c91e44802f14bf220134b1c7f19523f3ec9762b5
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-05-09 22:16:47 +0000

    Se implementaron vistas, parciales y helpers para la accion search en el controlador cite_index. Y otros cambios menores.
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@227 85877316-7822-0410-bcce-81226501b1f5

--- ScieloIndex/app/controllers/cite_index_controller.rb
+++ ScieloIndex/app/controllers/cite_index_controller.rb
@@ -3,22 +3,22 @@ class CiteIndexController < ApplicationController
   def index
     render :action => 'index'
   end
- 
+
   verify :method => :post, :only => [ :search ],
-         :redirect_to => { :action => :index }
+  :redirect_to => { :action => :index }
 
   def search
     @authors = Author.find(:all, :conditions => params[:record])
-    @articles = [ ]
+    @collection = [ ]
     @authors.each { |author|
-	author.articles.each { |article|
- 		@articles.push(article)
-	}	
-     }
-   if @articles > 0
-    render :action => 'results'
+      author.articles.each { |article|
+        @collection.push(article)
+      }
+    }
+   if @collection.size > 0
+     render :action => 'results'
    else
-    render :action => 'index'
+     render :action => 'index'
    end
-  end	
+  end
 end
--- ScieloIndex/app/helpers/cite_index_helper.rb
+++ ScieloIndex/app/helpers/cite_index_helper.rb
@@ -1,2 +1,23 @@
 module CiteIndexHelper
+  def apply_article_style(record, style="vancouver")
+    author_style(record.authors) + ', ' + record.title + ', ' + journal_style(record) +
+    ', ' + journal_issue_style(record)
+  end
+
+  def author_style(authors)
+    authors.collect { |author|
+      author_name = [ author.lastname]
+      author_name <<  author.firstname.first.upcase +
+        author.middlename.split(' ').collect { |name| name.first }.flatten.to_s
+      author_name.join(' ')
+    }.join(', ')
+  end
+
+  def journal_style(article)
+    article.journal_issue.journal.title
+  end
+
+  def journal_issue_style(article)
+    article.journal_issue.year.to_s
+  end
 end
