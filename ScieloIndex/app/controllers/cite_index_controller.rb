a88bfe9ff325ed423fd113e0b53fa80d5142692d
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-05-22 03:51:33 +0000

    Se hizo el paginador para los resultados de los articulos.
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@260 85877316-7822-0410-bcce-81226501b1f5

--- ScieloIndex/app/controllers/cite_index_controller.rb
+++ ScieloIndex/app/controllers/cite_index_controller.rb
@@ -1,20 +1,25 @@
 class CiteIndexController < ApplicationController
 
   def index
+    sessions[:seach_data] = nil
     render :action => 'index'
   end
 
   #verify :method => :post, :only => [ :search ],
   #:redirect_to => { :action => :index }
 
-  def search
+  def search_by_author
+    sessions[:seach_data] ||= Author.new(params[:record])
+    record = session[:search_data]
     @record = params[:record]
-    @pages, @collection = paginate Inflector.pluralize(Article).to_sym, :per_page => 10, :joins =>
-      "JOIN article_authors ON articles.id = article_authors.author_id JOIN authors ON authors.id = article_authors.author_id " +
-      "WHERE (authors.middlename = '#{params[:record][:middlename]}' AND authors.lastname = '#{params[:record][:lastname]}' AND authors.firstname = '#{params[:record][:firstname]}')",
-    :select => "authors.id, authors.firstname, authors.middlename, authors.lastname, " +
-      "articles.id, articles.title, articles.journal_issue_id, articles.fpage, articles.lpage, " +
-      "articles.page_range, articles.url, articles.pacsnum, articles.other"
+    @pages, @collection = paginate Inflector.pluralize(Article).to_sym,
+    :select => 'articles.id, articles.title, articles.journal_issue_id, articles.fpage, articles.lpage,' +
+    ' articles.page_range, articles.url, articles.pacsnum, articles.other, authors.id as author_id,' +
+    ' authors.firstname, authors.lastname, authors.middlename',
+    :joins => 'JOIN article_authors ON articles.id = articles_authors.article_id JOIN authors ON' +
+    " authors.id = articles_authors.author_id WHERE (authors.middlename = '#{record.middlename}' AND" +
+    " authors.lastname = '#{record.lastname}' AND authors.firstname = '#{record.firstname}')", :per_page => 10
+
   end
 
   def search_cites
--- ScieloIndex/app/helpers/cite_index_helper.rb
+++ ScieloIndex/app/helpers/cite_index_helper.rb
@@ -1,11 +1,9 @@
 module CiteIndexHelper
-  def pdf_download_link(article_id)
-    article = Article.find(article_id)
+  def pdf_download_link(article)
     link_to "Download PDF", :controller => 'associated_files', :action => 'send_file', :id => article.associated_file, :format => 'PDF'
   end
 
-  def html_view_link(article_id)
-    article = Article.find(article_id)
+  def html_view_link(article)
     link_to "View HTML", :controller => 'associated_files', :action => 'send_file', :id => article.associated_file, :format => 'HTML'
   end
 end
