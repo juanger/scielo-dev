599891c2d929f60dbf6c303a38132dc9419a9b6c
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-05-23 03:40:31 +0000

    Falto subir la prueba de integracion de subject.
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@271 85877316-7822-0410-bcce-81226501b1f5

---
+++ ScieloIndex/test/integration/subjects_test.rb
@@ -0,0 +1,62 @@
+require "#{File.dirname(__FILE__)}/../test_helper"
+
+class SubjectsTest < ActionController::IntegrationTest
+  fixtures :subjects
+
+  def setup
+    @subjects = [:geo, :atm, :bioqui, :fisica, :quimica]
+    @mysubject = {:parent_id => 1, :name => 'Mecanica Cuantica'}
+  end
+
+   def test_getting_index
+     get "/subjects"
+     assert_equal 200, status
+     assert_equal '/subjects', path
+   end
+
+   def test_new
+     get "/subjects/new"
+     assert_equal 200, status
+     assert_equal '/subjects/new', path
+   end
+
+   def  test_creating_new_subjects
+     post "subjects/create", :record => @mysubject
+     assert_equal 302, status
+     follow_redirect!
+     assert_equal '/subjects/list', path
+   end
+
+   def test_showing
+     @subjects.each { | subject |
+       post "/subjects/show", :id => subjects(subject).id
+       assert 200, status
+       assert_equal '/subjects/show', path
+     }
+   end
+
+   def test_editing
+     @subjects.each { | subject |
+       post "/subjects/edit", :id => subjects(subject).id
+       assert 200, status
+       assert "/subjects/edit/#{subjects(subject).id}", path
+     }
+   end
+
+   def test_updating_name
+     @subjects.each { | subject |
+       post "/subjects/update", :id => subjects(subject).id, :parent => subjects(subject).name.reverse
+       assert 302, status
+       follow_redirect!
+       assert "/subjects/show/#{subjects(subject).id}", path
+     }
+   end
+
+    def test_deleting
+     @subjects.each { | subject |
+       get "/subjects/destroy", :id => subjects(subject).id
+       assert 302, status
+       assert '/subjects/list',  path
+     }
+   end
+end
