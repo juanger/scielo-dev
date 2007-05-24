04cee384c21aab3722bca99b763754ae174135e7
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-05-24 23:18:43 +0000

    Ver los ChangeLog respectivos.
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@277 85877316-7822-0410-bcce-81226501b1f5

--- ScieloIndex/app/controllers/cite_index_controller.rb
+++ ScieloIndex/app/controllers/cite_index_controller.rb
@@ -3,6 +3,7 @@ class CiteIndexController < ApplicationController
   auto_complete_for :author, :middlename
   auto_complete_for :author, :lastname
   auto_complete_for :article, :title
+  auto_complete_for :keyword, :name
 
   def index
     session[:search_data] = nil
@@ -29,37 +30,56 @@ class CiteIndexController < ApplicationController
   end
 
   def find_article
-    session[:search_data] = Article.find(:first, :conditions => params[:article])
-    if session[:search_data].nil?
-      flash[:notice] = "Article not found, please try another search"
-      redirect_to :action => 'index'
-    else
-      redirect_to :action => 'search_by_title'
-    end
+    session[:search_data] = params[:article]
+    redirect_to :action => 'search_by_article'
+  end
+
+  def find_keyword
+    session[:search_data] = params[:keyword]
+    redirect_to :action => 'search_by_keyword'
   end
 
   def search_by_author
     @author = session[:search_data]
     @pages, @collection = paginate Inflector.pluralize(Article).to_sym,
-    :select => 'articles.id, articles.title, articles.journal_issue_id, articles.fpage, articles.lpage,' +
-    ' articles.page_range, articles.url, articles.pacsnum, articles.other, authors.id as author_id,' +
-    ' authors.firstname, authors.lastname, authors.middlename',
+    :select => 'articles.*',
     :joins => "JOIN article_authors ON articles.id = article_authors.article_id JOIN authors ON" +
-    " authors.id = article_authors.author_id WHERE (authors.middlename = '#{@author.middlename}' AND" +
-    " authors.lastname = '#{@author.lastname}' AND authors.firstname = '#{@author.firstname}')", :per_page => 10
+    " authors.id = article_authors.author_id",
+    :conditions => ["authors.middlename = :middlename AND authors.lastname = :lastname AND authors.firstname = :firstname",
+                    {:middlename => @author.middlename, :lastname => @author.lastname, :firstname => @author.firstname }],
+    :per_page => 10
   end
 
-  def search_by_title
+  def search_by_article
     @article = session[:search_data]
     @pages, @collection = paginate Inflector.pluralize(Article).to_sym,
-    :conditions => "articles.id = #{@article.id}",
+    :conditions => @article,
     :per_page => 10
+
+    if @collection.empty?
+      flash[:notice] = "No matches found, please try another search"
+      redirect_to :action => 'index'
+    end
+  end
+
+  def search_by_keyword
+    @keyword = session[:search_data]
+    @pages, @collection = paginate Inflector.pluralize(Article).to_sym,
+    :select => 'articles.*',
+    :joins => "JOIN article_keywords ON articles.id = article_keywords.article_id JOIN keywords ON" +
+    " keywords.id = article_keywords.keyword_id",
+    :conditions => ["keywords.name = :name", {:name => @keyword[:name]}],
+    :per_page => 10
+
+    if @collection.empty?
+      flash[:notice] = "No matches found, please try another search"
+      redirect_to :action => 'index'
+    end
   end
 
   def search_cites
     @pages, @collection = paginate Inflector.pluralize(Article).to_sym,
-    :select => 'articles.id, articles.title, articles.journal_issue_id, articles.fpage, articles.lpage,' +
-      ' articles.page_range, articles.url, articles.pacsnum, articles.other',
+    :select => 'articles.*',
     :joins => "JOIN cites ON articles.id = cites.cited_by_article_id",
     :conditions => "article_id = #{params[:id]}",
     :per_page => 10
