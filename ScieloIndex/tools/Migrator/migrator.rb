5f54b0e0bf9243c630c01ceebc07c406b191c9c7
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-06-11 23:44:05 +0000

    Se agregaron el archivo de ejemplo para la configuración del migrador y el script que hará de migrador
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@310 85877316-7822-0410-bcce-81226501b1f5

--- ScieloIndex/tools/Migrator/migrator.rb
+++ ScieloIndex/tools/Migrator/migrator.rb
@@ -1,20 +1,100 @@
-require "rubygems"
-gem "activerecord"
-require 'active_record'
-require 'yaml'
+#!/usr/bin/env ruby
+RAILS_ENV = 'development'
+require File.dirname(__FILE__) + '/../../config/environment'
 
-ActiveRecord::Base.establish_connection(
-  :adapter  => "postgresql",
-  :host     => "localhost",
-  :database => "scielo_development",
-  :username => "scielo",
-  :password => "" 
-)
+class Migrator
+  def initialize
+    config = "./config"
+    if File.file? config
+      open(config, "r") { |file|
+        file.each { |line|
+          next if line =~ /^#.*$/
+          obtain_value line}
+      }
+    else
+      puts "Por favor crear un archivo config."
+    end
+  end
 
-class Country < ActiveRecord::Base
+  def obtain_value(line)
+    array = line.split(':')
+    case array[0]
+      when 'SERIAL_ROOT'
+                @serial_root = array[1].strip
+                #puts 'SERIAL_ROOT ' + array[1]
+      when 'COUNTRY'
+                @origin_country = array[1].strip
+                #puts 'COUNTRY ' + array[1]
+      else
+                puts 'Archivo de configuración ilegal'
+    end
+  end
+
+  def process_scielo
+    if File.directory? @serial_root
+      Dir.foreach(@serial_root) { |dir|
+                next if dir =~ /^(\.|code|issue|issn|section|title|titleanterior)\.?$/
+
+                full_dir = File.join(@serial_root, dir)
+                if File.directory? full_dir
+                  @current_journal = dir
+                  process_journal(full_dir)
+                end
+      }
+    end
+  end
+
+  def process_journal(journal_dir)
+    puts "Migrando la revista: " + @current_journal
+
+    if File.directory? journal_dir
+      Dir.foreach(journal_dir) { |issue_dir|
+                next if issue_dir =~ /^(\.|_notes|paginasinformativas)\.?$/
+
+                full_dir = File.join(journal_dir, issue_dir)
+                if File.directory? full_dir
+                  @current_issue = issue_dir
+                  process_issue(full_dir)
+                end
+      }
+    end
+  end
+
+  def process_issue(issue_dir)
+    puts "Migrando numero: " + @current_issue
+    pdf_dir = File.join(issue_dir, "pdf")
+
+    if File.directory? pdf_dir
+      Dir.foreach(pdf_dir) { |pdf|
+        full_dir = File.join(pdf_dir, pdf)
+        next unless pdf =~ /^.*(\.)(pdf|PDF)$/
+
+        if File.file? full_dir
+          @current_article = pdf
+          process_pdf(full_dir)
+        end
+      }
+    else
+      puts "El numero no contiene documentos PDF"
+    end
+  end
+
+  def process_pdf(pdf_dir)
+    puts "Procesando articulo: " + @current_article
+  end
 end
 
-Country.find(:all).each {|country|
-  puts "Id: #{country.id}, Nombre: #{country.name}, Code: #{country.code}"  
-}
+# Country.find(:all).each { |country|
+#   puts "Id: #{country.id}, Nombre: #{country.name}, Code: #{country.code}"
+# }
 
+# Publisher.find(:all).each { |publisher|
+#   puts "id: #{publisher.id}, Titulo: #{publisher.name}"
+# }
+
+# Journal.find(:all).each { |journal|
+#   puts "id: #{journal.id}, Title: #{journal.title}, Country: #{journal.country.name}, Publisher: #{journal.publisher.name}, Abbrev: #{journal.abbrev}, ISSN: #{journal.issn}"
+# }
+
+migrator = Migrator.new()
+migrator.process_scielo()
