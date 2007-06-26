44bbd55cca9caba6384ca449e8e27493931e0972
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-06-26 00:56:08 +0000

    Se agregó el archivo para las referencias associated_references.rb
    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@336 85877316-7822-0410-bcce-81226501b1f5

---
+++ ScieloIndex/tools/Migrator/associated_references.rb
@@ -0,0 +1,12 @@
+class AssociatedReferences
+
+  def initialize(back, cited_by_article_id)
+    @back = back
+    @cited_by_article_id = cited_by_article_id
+
+    #puts "DEBUG: #{@back}"
+
+    #match = //.match(@back)
+  end
+end
+
