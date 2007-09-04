03ff0652c0ba1e1ccf47e46a88d7383d4ddebf2e
Héctor E. Gómez Morales <hectoregm@gmail.com>
2007-09-04 23:21:39 +0000

    git-svn-id: svn+ssh://dev.scielo.unam.mx/var/svn/repo/source/trunk@425 85877316-7822-0410-bcce-81226501b1f5

---
+++ PDF2Scielo/PDF2Scielo.Gui/StyleSelect.cs
@@ -0,0 +1,27 @@
+//
+// Foo.cs: A class that implements
+//
+// Author:
+//   Hector E. Gomez Morales (hectoregm@gmail.com)
+//   Anaid V. Velazquez Rivera (anaidv@gmail.com)
+//   Virginia Teodosio Procopio (ainigriv_t@hotmail.com)
+//   Juan Germán Castañeda Echevarría (juanger@gmail.com)
+//
+// Copyright (C) 2007 UNAM DGB
+//
+
+using System;
+
+namespace Scielo.PDF2Scielo
+{
+	
+	
+	public partial class StyleSelect : Gtk.Dialog
+	{
+		
+		public StyleSelect()
+		{
+			this.Build();
+		}
+	}
+}
