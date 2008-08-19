d80f0abe15655c735233834b5373597a7cadc42c
Juan Germán Castañeda Echevarría <juanger@gmail.com>
2008-08-18 19:41:41 -0500

    Minimal change, resizing images

--- ScieloIndex/app/helpers/citation_index_helper.rb
+++ ScieloIndex/app/helpers/citation_index_helper.rb
@@ -1,7 +1,7 @@
 module CitationIndexHelper
   def pdf_download_link(article)
     if article.associated_file
-      link_to image_tag("dl_pdf.png"), :controller => 'associated_files', :action => 'send_file', :id => article.associated_file, :format => 'PDF'
+      link_to image_tag("dl_pdf.png", :width => '32'), :controller => 'associated_files', :action => 'send_file', :id => article.associated_file, :format => 'PDF'
     else
       nil
     end
@@ -9,7 +9,7 @@ module CitationIndexHelper
 
   def html_view_link(article)
     if article.associated_file
-      link_to image_tag("view_html.png"), :controller => 'associated_files', :action => 'send_file', :id => article.associated_file, :format => 'HTML'
+      link_to image_tag("view_html.png", :width => '32'), :controller => 'associated_files', :action => 'send_file', :id => article.associated_file, :format => 'HTML'
     else
       nil
     end
